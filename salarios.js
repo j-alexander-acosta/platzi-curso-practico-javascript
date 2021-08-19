0e256656708fd7dd9de318a3f977a59f1e6d0b80
Alexander_Acosta <alexanderacosta@unach.cl>
2021-08-19 18:04:20 -0400

    Creacion de promedio de salarios

---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+  name: "Juanita",
+  salary: 1000,
+});
+colombia.push({
+  name: "Natalia",
+  salary: 1300,
+});
+colombia.push({
+  name: "Alexander",
+  salary: 3300,
+});
+colombia.push({
+  name: "Leidy",
+  salary: 2000,
+});
+colombia.push({
+  name: "Stefanya",
+  salary: 2300,
+});
+colombia.push({
+  name: "Liseth",
+  salary: 2000,
+});
+colombia.push({
+  name: "Eliana",
+  salary: 1800,
+});
+colombia.push({
+  name: "Carolina",
+  salary: 3000,
+});
+colombia.push({
+  name: "Jose",
+  salary: 1300,
+});
+colombia.push({
+  name: "Angelica",
+  salary: 1500,
+});
+colombia.push({
+  name: "Diana",
+  salary: 1300,
+});
+colombia.push({
+  name: "Clea",
+  salary: 1500,
+});
+colombia.push({
+  name: "Pilar",
+  salary: 3000,
+});
+colombia.push({
+  name: "Sandra",
+  salary: 3000,
+});
+colombia.push({
+  name: "Kelia",
+  salary: 1500,
+});
+colombia.push({
+  name: "Sandy",
+  salary: 1500,
+});
+colombia.push({
+  name: "Michell",
+  salary: 1300,
+});
+colombia.push({
+  name: "Katherine",
+  salary: 1500,
+});
+colombia.push({
+  name: "Camila",
+  salary: 1500,
+});
+colombia.push({
+  name: "Yenny",
+  salary: 1300,
+});
